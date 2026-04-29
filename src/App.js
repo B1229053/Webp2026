80c1817435d04243450e2eae87006f512e5faa4f
B1229053 <kate0801@gmail.com>
2026-04-29 14:34:58 +0800

    完成 Ex10 React first project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const styleArgument = { fontSize: '100px', color: 'red' }; // 設定樣式 [cite: 1654]
+
+const changeText = (event) => {
+  event.target.innerText = event.target.innerText + "被點了"; // 點擊變化 [cite: 1717]
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={styleArgument} onClick={changeText}>
+        hello CGU!!
+      </h1>
     </div>
   );
 }
